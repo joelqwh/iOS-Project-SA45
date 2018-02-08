f05b95c886c2d689946d638a39246a3ad56ee9fc
joel <hahaidiot@gmail.com>
2018-02-08 15:53:09 +0800

    merged changes

--- iOS Project SA45 Yee Junyi & Qin Wenhao Joel/AppDelegate.swift
+++ iOS Project SA45 Yee Junyi & Qin Wenhao Joel/AppDelegate.swift
@@ -17,9 +17,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
-        let vc : LoginViewController = LoginViewController(nibName:"LoginViewController", bundle:nil)
-        window?.rootViewController = vc
         return true
     }
 
