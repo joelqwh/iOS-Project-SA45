058d0e68eab5b7358a47bdddb2a8400b019772a0
joel <hahaidiot@gmail.com>
2018-02-08 13:34:24 +0800

    Initial Commit

---
+++ iOS Project SA45 Yee Junyi & Qin Wenhao Joel/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  iOS Project SA45 Yee Junyi & Qin Wenhao Joel
+//
+//  Created by Joel Qin on 8/2/18.
+//  Copyright © 2018 Institute of Systems Science. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
